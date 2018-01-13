2f014f0cb2634afd47e18a820bce940888469442
ImmortalRabbit <demezhan1998@gmail.com>
2018-01-13 20:37:55 +0600

    Create 1a.java

---
+++ lesson1/1a.java
@@ -0,0 +1,18 @@
+import java.lang.Math; // headers MUST be above the first class
+
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+	int l, w, h, area, volume;
+    l = 5;
+    w = 3;
+    h = 2;
+    area = l*w;
+    volume = area * h;
+    System.out.println("here is an area: " + area);
+    System.out.print("here is a volume: " + volume);
+  }
+}
