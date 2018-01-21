c7ff97ca3c638324ad28e213cdc3941fb270e88c
ImmortalRabbit <demezhan1998@gmail.com>
2018-01-22 03:50:18 +0600

    Update ElectronicDeviceTest.java

--- lesson3/ElectronicDeviceTest.java
+++ lesson3/ElectronicDeviceTest.java
@@ -3,12 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package electronicdevices;
+package electrodevices;
 
-/**
- *
- * @author samal
- */
 public class ElectronicDeviceTest {
 public static void doSomething (ElectronicDevices devices){
     System.out.println(devices);
